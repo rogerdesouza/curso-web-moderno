e9a80ad68bbf36f5cc62602f6ec05fd5e7c07f10
roger <roger@policiacivil.pe.gov.br>
2022-02-17 15:08:50 -0300

    ajustes

---
+++ 01-inicio/05-desafio.js
@@ -0,0 +1,21 @@
+/**
+ * Criar dois blocos, um que exiba impares e outro que exiba pares de 1 a 10
+ */
+
+{
+    console.log('Bloco 1')
+    console.log('1')
+    console.log('3')
+    console.log('5')
+    console.log('7')
+    console.log('9')
+}
+
+{
+    console.log('Bloco 2')
+    console.log('2')
+    console.log('4')
+    console.log('6')
+    console.log('8')
+    console.log('10')
+}
