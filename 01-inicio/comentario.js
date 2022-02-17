3e148248861b507a455f45a5caa8a8e0286f1e87
roger <roger@policiacivil.pe.gov.br>
2022-02-17 14:18:49 -0300

    01-inicio v1

---
+++ 01-inicio/comentario.js
@@ -0,0 +1,11 @@
+// console é usado para exibir algo na tela do computador
+console.log('Bom dia!!!')
+console.log('Boa tarde!!!') // mais um exemplo aqui de console
+console.log('Boa noite!!!')
+// mais um teste aqui
+
+/**
+ * este é um comentario
+ * de multiplas linhas
+ * 
+ */
