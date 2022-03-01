2c5feb73921d6cab7aa9c2d25d023beb3250e369
roger <roger@policiacivil.pe.gov.br>
2022-03-01 10:28:17 -0300

    aula 48

---
+++ 02 - exercicios/01 - fundamentos/12 - objeto.js
@@ -0,0 +1,13 @@
+const prod1 = {}
+prod1.nome = 'Celular'
+prod1.preco = 4999.99
+prod1['desconto'] = 0.40
+
+console.log(prod1)
+
+const prod2 = {
+    nome: 'Camisa',
+    preco: 79.90
+}
+
+console.log(prod2)
